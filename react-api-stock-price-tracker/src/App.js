627ac47082f7107b47aa6ca7f4e5c0cc264da14f
amatyaro <amatyaro@mnstate.edu>
2021-11-13 18:38:21 -0600

    created function to fetch API

---
+++ react-api-stock-price-tracker/src/App.js
@@ -0,0 +1,30 @@
+import React,{useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
+
+
+
+function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+
+  return (
+    <div className="App">
+      <h1>API</h1>
+    </div>
+  );
+}
+
+export default App;
